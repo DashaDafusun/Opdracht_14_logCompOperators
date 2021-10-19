162f2e8d736d25ae08d3bae3761073f3602cab06
DashaDafusun <84176953+DashaDafusun@users.noreply.github.com>
2021-10-19 18:43:04 +0200

    Add files via upload

---
+++ opdracht_14_logCompOperators/script.js
@@ -0,0 +1,57 @@
+
+// Deel 1
+
+const age = 21;
+
+if (age < 18) {
+
+    console.log("Sorry, jij bent te jong...");
+
+} else if (age === 17) {
+
+    console.log("Sorry, wij zien jou volgend jaar!");
+
+
+} else if (age >= 18 && age <=25) {
+
+    console.log("Je krijgt 50% korting!");
+
+} if (age > 70) {
+
+    console.log("Jij krijgt ons respect!");
+};
+
+
+
+
+
+// Deel 2
+
+const firstName = 'Paul';
+
+
+if (firstName == 'Sarah' || firstName == 'Abraham'|| firstName == 'Bram' ) {
+
+    console.log('WooHoo Gefeliciteerd! Een gratis biertje voor Jarig Job!');
+
+} else {
+    console.log('Hartelijk Welkom!');
+};
+
+
+// Deel 3
+
+const totalAmount = 19;
+
+if (totalAmount >= 25 && totalAmount < 50) {
+
+    console.log('Yes! Bestel jij 25 euro voor drankjes? Jij verdient een bordje gratis vegabiterballen!');
+
+} else if (totalAmount >= 50 && totalAmount < 100) {
+
+    console.log('Wow 50 euro voor drankjes? Jij krijgt een gratis portie nachos!!!');
+
+} else if (totalAmount >=100) {
+
+    console.log('Ya man, meer dan 100 euro voor drankjes? Gratis fles champagne voor jou!');
+};
